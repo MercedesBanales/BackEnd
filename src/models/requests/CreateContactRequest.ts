e0fd0a0b42fa69a4e2386db4a2170d7c78838201
MercedesBanales <mercibanales@hotmail.com>
2024-07-10 14:02:40 -0300

    Created models

---
+++ src/models/requests/CreateContactRequest.ts
@@ -0,0 +1,7 @@
+export interface CreateContactRequest {
+    name: string;
+    address: string;
+    email: string;
+    phone: string;
+    imagePath: string;
+}
---
+++ src/models/responses/ContactResponse.ts
@@ -0,0 +1,7 @@
+export interface ContactResponse {
+    name: string;
+    email: string;
+    address: string;
+    phone: string;
+    imagePath: string;
+}
